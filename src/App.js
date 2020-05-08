18323e4fc7b202337d640f51b16a546c8d4832f5
Alex Ball <alex.ball@cantina.co>
2020-05-07 23:15:13 -0400

    Add linting and react-native-elements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import * as React from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { Image, Platform, StyleSheet, Text, View } from "react-native";
+import { Button, Card, Icon, ListItem, ThemeProvider } from "react-native-elements";
+
+const users = [
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg",
+  },
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg",
+  },
+  {
+    name: "brynn",
+    avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg",
+  },
+];
+
 
 const instructions = Platform.select({
   ios: `Press Cmd+R to reload,\nCmd+D or shake for dev menu`,
@@ -8,29 +25,74 @@ const instructions = Platform.select({
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.instructions}>To get started, edit App.js</Text>
-      <Text style={styles.instructions}>{instructions}</Text>
-    </View>
+    <ThemeProvider>
+      <Card title="CARD WITH DIVIDER">
+        {users.map((u, i) => {
+          return (
+            <View key={i} style={styles.user}>
+              <Image
+                style={styles.image}
+                resizeMode="cover"
+                source={{ uri: u.avatar }}
+              />
+              <Text style={styles.name}>{u.name}</Text>
+            </View>
+          );
+        })}
+      </Card>
+      <Card containerStyle={{ padding: 0 }}>
+        {users.map((u, i) => {
+          return (
+            <ListItem
+              key={i}
+              roundAvatar
+              title={u.name}
+              avatar={{ uri: u.avatar }}
+            />
+          );
+        })}
+      </Card>
+      <Card title="HELLO WORLD" image={require("../android/app/src/main/res/mipmap-xxxhdpi/ic_launcher.png")}>
+        <Text style={{ marginBottom: 10 }}>
+          The idea with React Native Elements is more about component structure
+          than actual design.
+        </Text>
+        <Button
+          icon={<Icon name="code" color="#ffffff" />}
+          buttonStyle={{
+            borderRadius: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            marginBottom: 0,
+          }}
+          title="VIEW NOW"
+        />
+      </Card>
+      <Button title="Hey" />
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
 });
